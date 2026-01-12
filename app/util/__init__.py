8bc47d6f55619f37a0f1c883f8a91d2d3a943559
ribeirolw-svg <ribeirolw@gmail.com>
2026-01-11 22:43:20 -0300

    Update __init__.py

--- app/util/__init__.py
+++ app/util/__init__.py
@@ -1,1 +1,27 @@
+from utilitario.history_store import HistoryStore
+from utilitario.analytics import (
+    load_events,
+    filter_events,
+    last_n_days,
+    count_by_type,
+    count_by_key,
+    time_series_daily_count,
+    numeric_summary,
+    group_numeric_by_key,
+    build_dashboard_snapshot,
+    query_events_for_table,
+)
 
+__all__ = [
+    "HistoryStore",
+    "load_events",
+    "filter_events",
+    "last_n_days",
+    "count_by_type",
+    "count_by_key",
+    "time_series_daily_count",
+    "numeric_summary",
+    "group_numeric_by_key",
+    "build_dashboard_snapshot",
+    "query_events_for_table",
+]
